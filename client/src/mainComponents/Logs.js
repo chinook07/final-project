f17dc729123495b5db8f79af7ba6767ed98cf054
“Nicola <nicola.z@gmx.com>
2022-06-09 17:06:20 -0400

    feed and visit logs done

--- client/src/mainComponents/Logs.js
+++ client/src/mainComponents/Logs.js
@@ -1,88 +1,142 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
 
 import { DinoContext } from "../DinoContext";
 import Spinner from "../littleComponents/Spinner";
+import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 
 const Logs = () => {
 
-    const { assets, ready, update, setUpdate } = useContext(DinoContext);
-    
+    const { assets, ready } = useContext(DinoContext);
+
+    const [minShowF, setMinShowF] = useState(0);
+    const [maxShowF, setMaxShowF] = useState(9);
+
+    const [minShowV, setMinShowV] = useState(0);
+    const [maxShowV, setMaxShowV] = useState(9);
+
+    const showLaterF = () => {
+        if (minShowF > 1) {
+            setMinShowF(minShowF - 10)
+            setMaxShowF(maxShowF - 10);
+        }
+    }
+
+    const showEarlierF = (reOrderedFeedTimes) => {
+        if (maxShowF < reOrderedFeedTimes.length + 1) {
+            setMinShowF(minShowF + 10);
+            setMaxShowF(maxShowF + 10)
+        }
+        
+    }
+
+    const showLaterV = () => {
+        if (minShowV > 1) {
+            setMinShowV(minShowV - 10);
+            setMaxShowV(maxShowV - 10)
+        }
+    }
+
+    const showEarlierV = (reOrderedVisitTimes) => {
+        if (maxShowV < reOrderedVisitTimes.length + 1) {
+            setMinShowV(minShowV + 10);
+            setMaxShowV(maxShowV + 10)
+        }
+    }
+
     if (ready) {
-        console.log(assets);
+
+        // Only the carnivores need to get fed. Find them.
 
         const carnivores = [];
-        assets.forEach((item, index) => {
+        assets.forEach(item => {
             item.lastFeedings &&
             carnivores.push(item)
         })
 
-        console.log(carnivores);
+        // Put them in order.
 
-        const allVisits = assets.map((item, index) => {
-            return item.lastVisits
+        const feedingTimesOrder = [];
+        carnivores.map(dino => {
+            dino.lastFeedings.map(feed => {
+                feedingTimesOrder.push(feed.time)
+            })
+        })
+        feedingTimesOrder.sort().reverse()
+
+        let reOrderedFeedTimes = [];
+        feedingTimesOrder.forEach((item) => {
+            carnivores.forEach((dino) => {
+                dino.lastFeedings.forEach((time) => {
+                    if (time.time === item) {
+                        reOrderedFeedTimes.push({time: time.time, species: dino.species})
+                    }
+                })
+            })
         })
 
-        console.log(allVisits);
+        // Now for the visiting times
+
+        const visitTimesOrder = [];
+        assets.map(dino => {
+            dino.lastVisits.map(visit => {
+                visitTimesOrder.push(visit.time)
+            })
+        })
+        visitTimesOrder.sort().reverse()
+
+        let reOrderedVisitTimes = [];
+        visitTimesOrder.forEach((item) => {
+            assets.forEach((dino) => {
+                dino.lastVisits.forEach((time) => {
+                    if (time.time === item) {
+                        reOrderedVisitTimes.push({time: time.time, species: dino.species, employee: time.employee})
+                    }
+                })
+            })
+        })
 
         return (
             <>
-                <main>
+                <Main>
                     <h1>Logs</h1>
-                    <ul>
+                    <LogTree>
+                        <h2>Last feeding times</h2>
+                        <AiOutlineUp size={25} onClick={showLaterF}/>
                         {
-                        carnivores.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    The {item.species.toLowerCase()} were fed at:
-                                    <span>
-                                        <ol>
-                                            {
-                                                item.lastFeedings.map((feeding, index) => {
-                                                    return (
-                                                        <li key={index}>
-                                                            {feeding.time}
-                                                        </li>
-                                                    )
-                                                })
-                                            }
-                                        </ol>
-                                    </span>
-                                </li>
+                            reOrderedFeedTimes.map((item, index) => {
+                                if (index >= minShowF && index <= maxShowF) {
+                                    return (
+                                        <Log key={index}>
+                                            <div>{item.time}</div>
+                                            <div>{item.species}</div>
+                                        </Log>
+                                    )
+                                }
                                 
-                            )
-                        })
-                    }
-                    </ul>
-
-                    <br></br>
-
-                    <ul>
+                            })
+                        }
+                        <AiOutlineDown size={25} onClick={() => showEarlierF(reOrderedFeedTimes)}/>
+                    </LogTree>
+                    <LogTree>
+                        <h2>Last visiting times</h2>
+                        <AiOutlineUp size={25} onClick={showLaterV}/>
                         {
-                        assets.map((item, index) => {
-                            return (
-                                <li key={index}>
-                                    The {item.species.toLowerCase()} were visited at:
-                                    <span>
-                                        <ol>
-                                            {
-                                                item.lastVisits.map((visit, index) => {
-                                                    return (
-                                                        <li key={index}>
-                                                            {visit.time} by {visit.employee}
-                                                        </li>
-                                                    )
-                                                })
-                                            }
-                                        </ol>
-                                    </span>
-                                </li>
-                                
-                            )
-                        })
-                    }
-                    </ul>
-                </main>
+                            reOrderedVisitTimes.map((item, index) => {
+                                if (index >= minShowV && index <= maxShowV) {
+                                    return (
+                                        <Log key={index}>
+                                            <div>{item.time}</div>
+                                            <div>{item.employee}</div>
+                                            <div>{item.species}</div>
+                                        </Log>
+                                    )
+                                }
+                            })
+                        }
+                        <AiOutlineDown size={25} onClick={() => showEarlierV(reOrderedVisitTimes)}/>
+                    </LogTree>
+                </Main>
             </>
         )
     } else {
@@ -93,6 +147,28 @@ const Logs = () => {
     
 }
 
-const Wrapper = styled.div``
+const Main = styled.main``
+
+const LogTree = styled.div`
+    border: 1px solid var(--c-yellow);
+    margin: 20px 20px 0;
+    h2 {
+        margin-bottom: 10px;
+        text-align: center;
+    }
+    svg {
+        cursor: pointer;
+        display: block;
+        margin: 10px auto;
+    }
+`
+
+const Log = styled.div`
+    background-color: var(--c-gray);
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 10px;
+    padding: 10px;
+`
 
 export default Logs;
